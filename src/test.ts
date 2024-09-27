1bc1e68749e779b6188ba06fc75aaf2b68d343d1
Shaik Hajira Sultana <hsultana@edvak.com>
2024-09-27 10:17:56 +0530

    validations for additional info added

--- src/test.ts
+++ src/test.ts
@@ -172,7 +172,7 @@ const payload1: any = {
 }
 
 const payload: any = {
-    "controlNumber": "428523774",
+    "controlNumber": "753098499",
     "submitter": {
         "organizationName": "Institutional Hospital",
         "contactInformation": {
@@ -181,21 +181,21 @@ const payload: any = {
         }
     },
     "receiver": {
-        "organizationName": "Zing Health UB- formerly Zing Choice HMO UB"
+        "organizationName": "BCBS Texas - Star and Chip UB (DOS 5/1/24 & after)"
     },
     "subscriber": {
-        "memberId": "432424",
+        "memberId": "123456789",
         "paymentResponsibilityLevelCode": "P",
-        "firstName": "John",
-        "lastName": "Cena",
+        "firstName": "Test 1",
+        "lastName": "John",
         "gender": "M",
-        "dateOfBirth": "19900918",
-        "policyNumber": "432424",
+        "dateOfBirth": "20001220",
+        "policyNumber": "123456789",
         "address": {
-            "address1": "3rd Street Promenade",
-            "city": "Santa Monica",
-            "state": "CA",
-            "postalCode": "90401"
+            "address1": "PO BOX 660044",
+            "city": "Dallas",
+            "state": "TX",
+            "postalCode": "75266"
         }
     },
     "providers": [
@@ -239,9 +239,9 @@ const payload: any = {
     ],
     "claimInformation": {
         "claimFilingCode": "ZZ",
-        "patientControlNumber": "C0918CA11144",
+        "patientControlNumber": "C0920CA11212",
         "claimChargeAmount": "0.00",
-        "placeOfServiceCode": "11",
+        "placeOfServiceCode": "23",
         "claimFrequencyCode": "1",
         "planParticipationCode": "A",
         "benefitsAssignmentCertificationIndicator": "Y",
@@ -257,69 +257,168 @@ const payload: any = {
         },
         "serviceLines": [
             {
-                "serviceDate": "20240918",
-                "serviceDateEnd": "20240918",
+                "serviceDate": "20240920",
+                "serviceDateEnd": "20240920",
                 "institutionalService": {
                     "procedureIdentifier": "HC",
                     "lineItemChargeAmount": "0",
-                    "procedureCode": "99202",
+                    "procedureCode": "96365",
+                    "measurementUnit": "UN",
+                    "serviceUnitCount": "1",
+                    "serviceLineRevenueCode": "0113",
+                    "compositeDiagnosisCodePointers": {
+                        "diagnosisCodePointers": [
+                            "1"
+                        ]
+                    },
+                    "procedureModifiers": [
+                        "LT",
+                        "",
+                        "",
+                        ""
+                    ]
+                }
+            },
+            {
+                "serviceDate": "20240920",
+                "serviceDateEnd": "20240920",
+                "institutionalService": {
+                    "procedureIdentifier": "HC",
+                    "lineItemChargeAmount": "0",
+                    "procedureCode": "99439",
+                    "measurementUnit": "UN",
+                    "serviceUnitCount": "1",
+                    "serviceLineRevenueCode": "0113",
+                    "compositeDiagnosisCodePointers": {
+                        "diagnosisCodePointers": []
+                    }
+                }
+            },
+            {
+                "serviceDate": "20240920",
+                "serviceDateEnd": "20240920",
+                "institutionalService": {
+                    "procedureIdentifier": "HC",
+                    "lineItemChargeAmount": "0",
+                    "procedureCode": "99283",
                     "measurementUnit": "UN",
                     "serviceUnitCount": "1",
                     "serviceLineRevenueCode": "0110",
                     "compositeDiagnosisCodePointers": {
                         "diagnosisCodePointers": []
-                    }
+                    },
+                    "procedureModifiers": [
+                        "25",
+                        "",
+                        "",
+                        ""
+                    ]
+                }
+            },
+            {
+                "serviceDate": "20240920",
+                "serviceDateEnd": "20240920",
+                "institutionalService": {
+                    "procedureIdentifier": "HC",
+                    "lineItemChargeAmount": "0",
+                    "procedureCode": "00834",
+                    "measurementUnit": "UN",
+                    "serviceUnitCount": "1",
+                    "serviceLineRevenueCode": "0185",
+                    "compositeDiagnosisCodePointers": {
+                        "diagnosisCodePointers": []
+                    },
+                    "procedureModifiers": [
+                        "0A",
+                        "",
+                        "",
+                        ""
+                    ]
                 }
             }
         ],
         "claimDateInformation": {
-            "statementBeginDate": "20240918",
-            "statementEndDate": "20240918",
-            "dischargeHour": "202409180000",
-            "admissionDateAndHour": "202409180000"
+            "statementBeginDate": "20240920",
+            "statementEndDate": "20240920",
+            "dischargeHour": "1450",
+            "admissionDateAndHour": "202409201432"
         },
         "claimCodeInformation": {
-            "admissionTypeCode": "2",
-            "patientStatusCode": "01",
-            "admissionSourceCode": "2"
+            "admissionTypeCode": "5",
+            "patientStatusCode": "03",
+            "admissionSourceCode": "6"
         },
         "principalDiagnosis": {
             "qualifierCode": "ABK",
-            "principalDiagnosisCode": "A054"
+            "principalDiagnosisCode": "A012"
         },
         "admittingDiagnosis": {
             "qualifierCode": "ABJ",
-            "admittingDiagnosisCode": "A038"
+            "admittingDiagnosisCode": "A0104"
         },
         "principalProcedureInformation": {
-            "principalProcedureCode": "99202",
-            "principalProcedureDateTime": "2024-09-18"
+            "principalProcedureCode": "96365",
+            "principalProcedureDateTime": "2024-09-20"
         },
         "diagnosisRelatedGroupInformation": {
-            "drugRelatedGroupCode": "6"
+            "drugRelatedGroupCode": "1"
         },
-        "otherDiagnosisInformationList": [
-            {
-                "qualifierCode": "ABF",
-                "otherDiagnosisCode": "A014"
-            },
-            {
-                "qualifierCode": "ABF",
-                "otherDiagnosisCode": "A011"
-            },
-            {
-                "qualifierCode": "ABF",
-                "otherDiagnosisCode": "A0104"
-            }
-        ],
         "conditionCodes": [
+            {
+                "conditionCode": "10"
+            },
+            {
+                "conditionCode": "19"
+            },
+            {
+                "conditionCode": "30"
+            },
             {
                 "conditionCode": "36"
             }
+        ],
+        "patientReasonForVisits": [
+            {
+                "qualifierCode": "APR",
+                "patientReasonForVisitCode": "A0104"
+            },
+            {
+                "qualifierCode": "APR",
+                "patientReasonForVisitCode": "A012"
+            }
+        ],
+        "occurrenceInformationList": [
+            {
+                "occurrenceSpanCode": "70",
+                "occurrenceSpanCodeDate": "20240925"
+            }
+        ],
+        "occurrenceSpanInformations": [
+            {
+                "occurrenceSpanCode": "70",
+                "occurrenceSpanCodeStartDate": "20240919",
+                "occurrenceSpanCodeEndDate": "20240926"
+            }
+        ],
+        "valueInformationList": [
+            {
+                "valueCode": "6",
+                "valueCodeAmount": "110.00"
+            }
+        ],
+        "externalCauseOfInjuries": [
+            {
+                "qualifierCode": "ABN",
+                "externalCauseOfInjury": "A012"
+            },
+            {
+                "qualifierCode": "ABN",
+                "externalCauseOfInjury": "A0104"
+            }
         ]
     },
-    "tradingPartnerServiceId": "U3248",
-    "tradingPartnerName": "Zing Health UB- formerly Zing Choice HMO UB"
+    "tradingPartnerServiceId": "66002",
+    "tradingPartnerName": "BCBS Texas - Star and Chip UB (DOS 5/1/24 & after)"
 }
 const result = generate837I(payload, 'V30Z')
 console.log('result: ', result);
